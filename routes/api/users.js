b570d4bb4bcc9352c5acde5dc97ee1d5e2883198
Jonathan Sorci <scottsorci@gmail.com>
2019-06-05 20:06:56 -0500

    routes and mongo config

---
+++ routes/api/users.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router();
+const { check, validationResult } = require('express-validator/check')
+
+
+// @route   POST api/users
+// @desc    Register User
+// @access  Public
+router.post('/', (req,res) => {
+    console.log(req.body)
+    res.send('User route')
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
-app.get('/',(req,res) => res.send('API Running'))
+
+//connect to db 
+connectDB();
+
+// init middleware
+app.use(express.json({ extended:false}))
+
+//define routes
+app.use('/api/user', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`))
